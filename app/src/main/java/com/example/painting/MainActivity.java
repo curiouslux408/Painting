993f6c6aa3d73a859683b67ad498e2c9a4766ee0
Edward <curiouslux408@emct.com.tw>
2020-08-12 10:09:22 +0800

    Create New Project

---
+++ app/src/main/java/com/example/painting/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.painting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentResolver;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private PaintView paintView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        paintView = (PaintView) findViewById(R.id.paintView);
+        //讀取手機長寬解析度、密度
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        paintView.init(metrics);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.normal:
+                paintView.normal();
+                return true;
+            case R.id.emboss:
+                paintView.emboss();
+                return true;
+            case R.id.blur:
+                paintView.blur();
+                return true;
+            case R.id.clear:
+                paintView.clear();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
